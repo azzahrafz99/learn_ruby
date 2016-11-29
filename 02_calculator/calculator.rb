0245783e818f576a36980f3dd0cc4bdb270f3bdf
Fatimah Az-Zahra <azzahrafz@gmail.com>
2016-11-29 14:41:01 +0700

    selesai sampai 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a-b
+end
+
+def sum(numbers)
+  total = 0
+  numbers.each do |a|
+    total+=a
+  end
+  total
+end
+
+def power(a, b)
+  a**b
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
 #write your code here
+def echo a
+  a
+end
+
+def shout(a)
+  a.upcase
+end
+
+def repeat(a)
+  2.times do |a|
+    p a + " " + a
+  end
+end
+
+def repeat(a, how_many=2)
+  str= a + " " + a
+  return str if how_many == 2
+  how_many-=2
+  how_many.times do
+     str += (" " +a)
+  end
+  str
+end
